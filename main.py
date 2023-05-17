cfeb3c33edf1df2f66d2f78f98fa8b51abe94027
Daksh Sinha <sinha.dsinha@gmail.com>
2023-05-16 23:32:34 -0700

    added MIDI encoding/decoding

--- main.py
+++ main.py
@@ -1,1 +1,257 @@
 # CLaMIDIa time
+import os
+import sys
+import io
+import zipfile
+import miditoolkit
+import random
+import time
+import math
+import signal
+import hashlib
+from multiprocessing import Pool, Lock, Manager
+
+
+pos_resolution = 16  # per beat (quarter note)
+bar_max = 256
+velocity_quant = 4
+tempo_quant = 12  # 2 ** (1 / 12)
+min_tempo = 16
+max_tempo = 256
+duration_max = 8  # 2 ** 8 * beat
+max_ts_denominator = 6  # x/1 x/2 x/4 ... x/64
+max_notes_per_bar = 2  # 1/64 ... 128/64
+beat_note_factor = 4  # In MIDI format a note is always 4 beats
+deduplicate = True
+filter_symbolic = False
+filter_symbolic_ppl = 16
+trunc_pos = 2 ** 16  # approx 30 minutes (1024 measures)
+sample_len_max = 1000  # window length max
+sample_overlap_rate = 4
+ts_filter = False
+pool_num = 24
+max_inst = 127
+max_pitch = 127
+max_velocity = 127
+
+ts_dict = dict()
+ts_list = list()
+for i in range(0, max_ts_denominator + 1):  # 1 ~ 64
+    for j in range(1, ((2 ** i) * max_notes_per_bar) + 1):
+        ts_dict[(j, 2 ** i)] = len(ts_dict)
+        ts_list.append((j, 2 ** i))
+dur_enc = list()
+dur_dec = list()
+for i in range(duration_max):
+    for j in range(pos_resolution):
+        dur_dec.append(len(dur_enc))
+        for k in range(2 ** i):
+            dur_enc.append(len(dur_dec) - 1)
+
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, exc_type, value, traceback):
+        signal.alarm(0)
+
+
+def t2e(x):
+    assert x in ts_dict, 'unsupported time signature: ' + str(x)
+    return ts_dict[x]
+
+
+def e2t(x):
+    return ts_list[x]
+
+
+def d2e(x):
+    return dur_enc[x] if x < len(dur_enc) else dur_enc[-1]
+
+
+def e2d(x):
+    return dur_dec[x] if x < len(dur_dec) else dur_dec[-1]
+
+
+def v2e(x):
+    return x // velocity_quant
+
+
+def e2v(x):
+    return (x * velocity_quant) + (velocity_quant // 2)
+
+
+def b2e(x):
+    x = max(x, min_tempo)
+    x = min(x, max_tempo)
+    x = x / min_tempo
+    e = round(math.log2(x) * tempo_quant)
+    return e
+
+
+def e2b(x):
+    return 2 ** (x / tempo_quant) * min_tempo
+
+
+def time_signature_reduce(numerator, denominator):
+    # reduction (when denominator is too large)
+    while denominator > 2 ** max_ts_denominator and denominator % 2 == 0 and numerator % 2 == 0:
+        denominator //= 2
+        numerator //= 2
+    # decomposition (when length of a bar exceed max_notes_per_bar)
+    while numerator > max_notes_per_bar * denominator:
+        for i in range(2, numerator + 1):
+            if numerator % i == 0:
+                numerator //= i
+                break
+    return numerator, denominator
+
+def MIDI_to_encoding(midi_obj):
+    def time_to_pos(t):
+        return round(t * pos_resolution / midi_obj.ticks_per_beat)
+    notes_start_pos = [time_to_pos(j.start)
+                       for i in midi_obj.instruments for j in i.notes]
+    if len(notes_start_pos) == 0:
+        return list()
+    max_pos = min(max(notes_start_pos) + 1, trunc_pos)
+    pos_to_info = [[None for _ in range(4)] for _ in range(
+        max_pos)]  # (Measure, TimeSig, Pos, Tempo)
+    tsc = midi_obj.time_signature_changes
+    tpc = midi_obj.tempo_changes
+    for i in range(len(tsc)):
+        for j in range(time_to_pos(tsc[i].time), time_to_pos(tsc[i + 1].time) if i < len(tsc) - 1 else max_pos):
+            if j < len(pos_to_info):
+                pos_to_info[j][1] = t2e(time_signature_reduce(
+                    tsc[i].numerator, tsc[i].denominator))
+    for i in range(len(tpc)):
+        for j in range(time_to_pos(tpc[i].time), time_to_pos(tpc[i + 1].time) if i < len(tpc) - 1 else max_pos):
+            if j < len(pos_to_info):
+                pos_to_info[j][3] = b2e(tpc[i].tempo)
+    for j in range(len(pos_to_info)):
+        if pos_to_info[j][1] is None:
+            # MIDI default time signature
+            pos_to_info[j][1] = t2e(time_signature_reduce(4, 4))
+        if pos_to_info[j][3] is None:
+            pos_to_info[j][3] = b2e(120.0)  # MIDI default tempo (BPM)
+    cnt = 0
+    bar = 0
+    measure_length = None
+    for j in range(len(pos_to_info)):
+        ts = e2t(pos_to_info[j][1])
+        if cnt == 0:
+            measure_length = ts[0] * beat_note_factor * pos_resolution // ts[1]
+        pos_to_info[j][0] = bar
+        pos_to_info[j][2] = cnt
+        cnt += 1
+        if cnt >= measure_length:
+            assert cnt == measure_length, 'invalid time signature change: pos = {}'.format(
+                j)
+            cnt -= measure_length
+            bar += 1
+    encoding = []
+    start_distribution = [0] * pos_resolution
+    for inst in midi_obj.instruments:
+        for note in inst.notes:
+            if time_to_pos(note.start) >= trunc_pos:
+                continue
+            start_distribution[time_to_pos(note.start) % pos_resolution] += 1
+            info = pos_to_info[time_to_pos(note.start)]
+            encoding.append((info[0], info[2], max_inst + 1 if inst.is_drum else inst.program, note.pitch + max_pitch +
+                             1 if inst.is_drum else note.pitch, d2e(time_to_pos(note.end) - time_to_pos(note.start)), v2e(note.velocity), info[1], info[3]))
+    if len(encoding) == 0:
+        return list()
+    tot = sum(start_distribution)
+    start_ppl = 2 ** sum((0 if x == 0 else -(x / tot) *
+                          math.log2((x / tot)) for x in start_distribution))
+    # filter unaligned music
+    if filter_symbolic:
+        assert start_ppl <= filter_symbolic_ppl, 'filtered out by the symbolic filter: ppl = {:.2f}'.format(
+            start_ppl)
+    encoding.sort()
+    return encoding
+
+def encoding_to_MIDI(encoding):
+    # TODO: filter out non-valid notes and error handling
+    bar_to_timesig = [list()
+                      for _ in range(max(map(lambda x: x[0], encoding)) + 1)]
+    for i in encoding:
+        bar_to_timesig[i[0]].append(i[6])
+    bar_to_timesig = [max(set(i), key=i.count) if len(
+        i) > 0 else None for i in bar_to_timesig]
+    for i in range(len(bar_to_timesig)):
+        if bar_to_timesig[i] is None:
+            bar_to_timesig[i] = t2e(time_signature_reduce(
+                4, 4)) if i == 0 else bar_to_timesig[i - 1]
+    bar_to_pos = [None] * len(bar_to_timesig)
+    cur_pos = 0
+    for i in range(len(bar_to_pos)):
+        bar_to_pos[i] = cur_pos
+        ts = e2t(bar_to_timesig[i])
+        measure_length = ts[0] * beat_note_factor * pos_resolution // ts[1]
+        cur_pos += measure_length
+    pos_to_tempo = [list() for _ in range(
+        cur_pos + max(map(lambda x: x[1], encoding)))]
+    for i in encoding:
+        pos_to_tempo[bar_to_pos[i[0]] + i[1]].append(i[7])
+    pos_to_tempo = [round(sum(i) / len(i)) if len(i) >
+                    0 else None for i in pos_to_tempo]
+    for i in range(len(pos_to_tempo)):
+        if pos_to_tempo[i] is None:
+            pos_to_tempo[i] = b2e(120.0) if i == 0 else pos_to_tempo[i - 1]
+    midi_obj = miditoolkit.midi.parser.MidiFile()
+
+    def get_tick(bar, pos):
+        return (bar_to_pos[bar] + pos) * midi_obj.ticks_per_beat // pos_resolution
+    midi_obj.instruments = [miditoolkit.containers.Instrument(program=(
+        0 if i == 128 else i), is_drum=(i == 128), name=str(i)) for i in range(128 + 1)]
+    for i in encoding:
+        start = get_tick(i[0], i[1])
+        program = i[2]
+        pitch = (i[3] - 128 if program == 128 else i[3])
+        duration = get_tick(0, e2d(i[4]))
+        if duration == 0:
+            duration = 1
+        end = start + duration
+        velocity = e2v(i[5])
+        midi_obj.instruments[program].notes.append(miditoolkit.containers.Note(
+            start=start, end=end, pitch=pitch, velocity=velocity))
+    midi_obj.instruments = [
+        i for i in midi_obj.instruments if len(i.notes) > 0]
+    cur_ts = None
+    for i in range(len(bar_to_timesig)):
+        new_ts = bar_to_timesig[i]
+        if new_ts != cur_ts:
+            numerator, denominator = e2t(new_ts)
+            midi_obj.time_signature_changes.append(miditoolkit.containers.TimeSignature(
+                numerator=numerator, denominator=denominator, time=get_tick(i, 0)))
+            cur_ts = new_ts
+    cur_tp = None
+    for i in range(len(pos_to_tempo)):
+        new_tp = pos_to_tempo[i]
+        if new_tp != cur_tp:
+            tempo = e2b(new_tp)
+            midi_obj.tempo_changes.append(
+                miditoolkit.containers.TempoChange(tempo=tempo, time=get_tick(0, i)))
+            cur_tp = new_tp
+    return midi_obj
+
+if __name__ == '__main__':
+    # (0 Measure, 1 Pos, 2 Program, 3 Pitch, 4 Duration, 5 Velocity, 6 TimeSig, 7 Tempo)
+    filename = 'dq.mid'
+    with open(filename, 'rb') as f:
+            midi_file = io.BytesIO(f.read())
+    midi_obj = miditoolkit.midi.parser.MidiFile(file=midi_file)
+    enc = MIDI_to_encoding(midi_obj)
+    print(len(enc), type(enc))
+    print(enc[0], enc[1])
+    dec = encoding_to_MIDI(enc)
+    print(type(dec))
